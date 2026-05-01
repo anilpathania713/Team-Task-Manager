d80aec1e436fb3684bf61b53ae8f4bf499c54ef1
anilpathania712 <anilpathania712@gmail.com>
2026-05-01 14:57:55 +0530

    test

--- server/app.js
+++ server/app.js
@@ -28,6 +28,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
+console.log("ENV PORT:", process.env.PORT);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
