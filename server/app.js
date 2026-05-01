46ef8629d89b11c5fc5fbed1688a6d8d3baa6b03
anilpathania712 <anilpathania712@gmail.com>
2026-05-01 12:14:54 +0530

    minor change

--- server/app.js
+++ server/app.js
@@ -1,22 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
-const { auth } = require("./middleware/auth");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://your-frontend-name.up.railway.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
-const db = require("./config/db");
+require("./config/db");
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/projects", projectRoutes);
-
 app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
